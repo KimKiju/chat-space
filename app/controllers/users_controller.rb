b6f85cbf83124f7f2e8bfdac474fc0b502452136
kim kiju <kimkiju.1216@gmail.com>
2017-05-24 02:15:28 +0900

    user_update,sign_in,sign_out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def update
+    User.update(update_params)
+    redirect_to controller: 'messages', action: 'index'
+  end
+
+private
+  def update_params
+    params.require(:user).permit(:name,:email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :users, only:[ :update ]
   resources :messages
+  devise_for :users
   root 'messages#index'
 end
